220e7342013a8d72f12082161d6f3e7c48862b10
WyrdsumStudios <wyrdsumstudios@outlook.com>
2026-05-03 08:00:11 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -17,11 +18,8 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-    
     if bot.user.mentioned_in(message):
-        await message.channel.typing()
-        await message.reply("Vesperath is here. What do you need, my friend?")
-    
+        await message.reply("Vesperath is here. What do you need?")
     await bot.process_commands(message)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
